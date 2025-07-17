87c28f1904897d8ab64121ea99691d0317da5d82
Marcel Niemiec <marcel.niemiec6@gmail.com>
2025-07-17 21:30:18 +0200

    fix: odometer issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
-import OdometerLoader from '@/components/Odometer'
 import Script from 'next/script'
 import './globals.css'
+import OdometerLoader from '@/components/Odometer'
 
 export const metadata: Metadata = {
   title: 'Serwis Motocyklowy',
@@ -12,16 +12,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-
-  
-
   return (
     <html lang="en">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-default.min.css" />
-
-      <OdometerLoader />
-      <body>      
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.7/odometer.min.js" strategy="beforeInteractive"/> 
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.8/themes/odometer-theme-default.min.css"
+          precedence="default"
+        />
+      </head>
+      <body>
+        <Script
+          src="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.8/odometer.min.js"
+          strategy="beforeInteractive"
+        />
+        <OdometerLoader />
         {children}
       </body>
     </html>
